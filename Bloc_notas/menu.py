cf71fa7d31f0991a257c04430b9341f129b63ca6
Rommel <momerg2016@gmail.com>
2025-02-15 11:56:39 -0400

    function_done

--- Bloc_notas/menu.py
+++ Bloc_notas/menu.py
@@ -10,24 +10,51 @@ ventana.config(bg= "gray13")
 ventana.resizable(False,False)
 
 #VARIABLE AUXILIAR
-ruta_archivo = "" 
+ruta_archivo = ""
 
-#FUNCIONES 
+#FUNCIONE PARA REESTABLECER
 def handlNew_file():
     text_entry.delete(1.0, tk.END)
 
+#FUNCION PARA CARGAR ARCHIVOS TEXTOS
 def handlOpen():
     global ruta_archivo
     ruta_archivo = filedialog.askopenfilename(defaultextension= ".TXT", 
                                                 filetypes = [("Archivos de texto", "*.TxT"),
                                                             ("Archivos python", "*.py"),
-                                                            ("Todo tipo de archivo","*.*")])
+                                                            ("Todo tipo de archivo", "*.*")])
     
     with open(ruta_archivo, "r", encoding= "utf-8") as file:
         text_entry.insert(tk.INSERT, file.read())
 
+#FUNCION PARA GUARDAR
 def handlSave():
-    print("Guardando...")
+    global ruta_archivo
+    ruta_archivo = filedialog.asksaveasfilename(defaultextension= ".TXT", 
+                                                filetypes = [("Archivos de texto", "*.TxT"),
+                                                            ("Archivos python", "*.py"),
+                                                            ("Todo tipo de archivo", "*.*")])
+    if ruta_archivo:
+        try:
+
+            with open(ruta_archivo, "w", encoding= "utf-8") as file:
+                file.write(text_entry.get(1.0, tk.END))
+        
+        except:
+            print("No se guardo el archivo")
+
+#FUNCIONES PARA COPIAR, PEGAR, Y CORTAR TEXTO
+def handlCopy():
+    text_entry.event_generate("<<Copy>>")
+    print("Copiado")
+
+def handlPaste():
+    text_entry.event_generate("<<Paste>>")
+    print("Pegado")
+
+def handlCut():
+    text_entry.event_generate("<<Cut>>")
+    print('Cortado')
 
 #CREACION DE LA BARRA MENU
 barra_menu = tk.Menu(ventana)
@@ -49,10 +76,10 @@ editar_menu = tk.Menu(barra_menu)
 barra_menu.add_cascade(label= "Editar", menu= editar_menu)
 
 #COMANDOS AGREGADOS AL MENU EDITAR
-editar_menu.add_command(label= "Copiar")
-editar_menu.add_command(label="Pegar")
+editar_menu.add_command(label= "Copiar", command= handlCopy)
+editar_menu.add_command(label="Pegar", command= handlPaste)
 editar_menu.add_separator()
-editar_menu.add_command(label= "Cortar")
+editar_menu.add_command(label= "Cortar", command= handlCut)
 
 #ENTRADA DE MAYOR TEXTO
 text_entry = tk.Text(ventana)
